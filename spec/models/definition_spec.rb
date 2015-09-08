49143811f6769d925173a5254b61b254eeb51b48
Franze Jr <franzejr@gmail.com>
2015-09-08 19:05:20 -0300

    Using should for validations

--- spec/models/definition_spec.rb
+++ spec/models/definition_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Definition, type: :model do
   describe 'validations' do
-		it {is_expected. to validate_presence_of(:name)}
+		it { should validate_presence_of(:name)}
 		it { should belong_to(:word) }
 	end
 end
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Word, type: :model do
   describe 'validations' do
-		it {is_expected. to validate_presence_of(:name)}
+		it { should validate_presence_of(:name)}
 		it { should have_many(:definitions) }
 	end
 end
