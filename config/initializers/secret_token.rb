a336042726c3915fdd79f654f59106044112605c
Franze Jr <franzejr@gmail.com>
2015-09-08 17:48:20 -0300

    Changing folder /spa to doogle_api folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+DoogleApi::Application.config.secret_token = '2305d092c751e2ce9beb13fe948c6b210abfc5484ae1453e9f94ddc8ea24cfff18ded7df2317189652698b3911ac035b6d98fe85f6667ec98dfc87531def9ff7'
