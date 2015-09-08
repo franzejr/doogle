77766a23873cef27ce76380a88a367b783cb7029
Franze Jr <franzejr@gmail.com>
2015-09-08 18:38:13 -0300

    Creating serializer for word and starting words_controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,18 @@
 class WordsController < ApplicationController
+
+	def index
+		render json: Word.all
+	end
+
+	def show
+		find_word
+		render json: @word
+	end
+
+	private
+
+	def find_word
+		@word = Word.find_by(name: params[:name])
+	end
+
 end
