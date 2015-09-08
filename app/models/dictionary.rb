3b2a7ff93088513041d5eac7f8b9ae83ac37c6db
Franze Jr <franzejr@gmail.com>
2015-09-08 19:44:52 -0300

    Implementing a method definitions for the Dictionary

--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -1,19 +1,24 @@
 module Dictionary
-	class Word
-		attr_reader :xml
+  class Word
+    attr_reader :xml
 
-		API_DICTIONARY_URL = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/"
+    API_DICTIONARY_URL = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/"
 
-		def initialize(word)
-			word_url = generate_url_for(word)
-			@xml = Nokogiri::HTML(open(word_url))
-		end
+    def initialize(word)
+      word_url = generate_url_for(word)
+      @xml = Nokogiri::HTML(open(word_url))
+    end
+
+    def definitions
+      @xml.xpath("//entry_list/entry/def/dt").map{|def| def.content}
+    end
 
 
-		private
+   private
 
-		def generate_url_for(word, url = API_DICTIONARY_URL)
-			"#{url}#{word}?key=#{ENV['API_KEY']}"
-		end
+   def generate_url_for(word, url = API_DICTIONARY_URL)
+     "#{url}#{word}?key=#{ENV['API_KEY']}"
+   end
+
 	end
 end
